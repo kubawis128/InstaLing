7a1afd76251b40f4ef54e92bef14cd6808ea5512
kubawis128 <j2006wis1@gmail.com>
2021-09-28 09:33:30 +0200

    New UI \o/

--- gui.py
+++ gui.py
@@ -1,24 +1,26 @@
-from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk
+import tkinter as tk
 import handler
 import threading
-
+from ttkthemes import ThemedTk
 autorunning = False
 
 def init():
     global btn2
     global window
-    window = Tk()
+    window = ThemedTk(theme='winxpblue')
     window.title("Instaling bot")
     window.geometry('350x200')
-    btn = Button(window, text="Init hander", command=initHandler)
-    btn.grid(column=2, row=0)
-    btn1 = Button(window, text="Solve task", command=solve)
-    btn1.grid(column=3, row=0)
-    btn2 = Button(window, text="Start Auto Mode", command=auto_solve)
-    btn2.grid(column=4, row=0)
-    btn3 = Button(window, text="Quit", command=exit)
-    btn3.grid(column=5, row=0)
+    s = ttk.Style()
+    s.configure('TButton', foreground='white')
+    btn = ttk.Button(window, text="Init hander", command=initHandler)
+    btn.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+    btn1 = ttk.Button(window, text="Solve task", command=solve)
+    btn1.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+    btn2 = ttk.Button(window, text="Start Auto Mode", command=auto_solve)
+    btn2.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+    btn3 = ttk.Button(window, text="Quit", command=exit)
+    btn3.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
     window.mainloop()
 
 def initHandler():
