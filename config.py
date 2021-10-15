1027a34770a5f594bf7714835964cb0f1eab0f75
kubawis128 <j2006wis1@gmail.com>
2021-10-15 23:18:58 +0200

    gui for lang change

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import configparser
 config = configparser.ConfigParser()
 def loadConf():
     config.read('config.conf')
+def saveConf(selection,name,toSave):
+    config[selection][name] = str(toSave)
+    with open('config.conf','w') as configfile:
+        config.write(configfile)
 def getConf(selection,name):
     conf = config[selection]
     return conf.get(name)
--- gui.py
+++ gui.py
@@ -3,14 +3,19 @@ import tkinter as tk
 import handler
 import threading
 from ttkthemes import ThemedTk
+import config
+
 autorunning = False
 
 def init():
     global btn2
     global window
+    global lang
     window = ThemedTk(theme='arc')
     window.title("Instaling bot")
     window.geometry('350x200')
+    lang = tk.StringVar(window)
+    lang.set("en")
     #s = ttk.Style()
     #s.configure('TButton', foreground='white')
     btn = ttk.Button(window, text="Init hander", command=initHandler)
@@ -21,11 +26,16 @@ def init():
     btn2.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
     btn3 = ttk.Button(window, text="Quit", command=exit)
     btn3.place(relx=0.5, rely=0.8, anchor=tk.CENTER)
+    list = ttk.OptionMenu(window, lang, "en","en", "de",command=changeLang)
+    list.place(relx=0.1, rely=0.1, anchor=tk.CENTER)
     window.mainloop()
+    
 
 def initHandler():
     handler.init()
-
+def changeLang(hel = 0):
+    global lang
+    config.saveConf('translator','to',lang.get())
 def solve():
     handler.start()
 
